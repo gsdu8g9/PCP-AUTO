ba8331bf6c86ba89a0d438d33b1981a3d7e0708c
semenov-k <semenov.develop@gmail.com>
2016-11-24 00:53:01 +0500

    final fix api wrapper

--- modules/ok_api.py
+++ modules/ok_api.py
@@ -2,7 +2,7 @@ import requests
 import access_config as conf
 import hashlib
 import json
-
+import urllib
 
 def api_request(method, params={}):
     api_server = 'https://api.ok.ru/fb.do?'
@@ -19,46 +19,47 @@ def api_request(method, params={}):
     for key in sig_keys:
         if key == 'method':
             sig_string += 'method={}'.format(method)
-            request_string += '&method={}'.format(method)
         else:
             sig_string += '{}={}'.format(key, params[key])
-            request_string += '&{}={}'.format(key, params[key])
 
     sig_string += conf.SECRET_SESSION_KEY
-    sig_string = sig_string.replace('+', ' ')
     sig = hashlib.md5(sig_string.encode('utf8')).hexdigest()
     print(sig_string)
 
-    request_string = '{}access_token={}{}&application_key={}&sig={}' \
-        .format(api_server, conf.ACCESS_TOKEN, request_string, conf.PUBLIC_KEY, sig)
-    response = requests.get(request_string).text
+    main_params = {
+        'access_token': conf.ACCESS_TOKEN,
+        'application_key': conf.PUBLIC_KEY,
+        'sig': sig,
+        'method': method
+    }
+    url_string = api_server + urllib.parse.urlencode({**main_params, **params})
+    response = requests.get(url_string).text
     return response
 
 
 def post(img_path='../img/post.png'):
     response_json = api_request('photosV2.getUploadUrl', {'gid': '53233370661082'})
     upload_url = json.loads(response_json)['upload_url']
-    photos_json = requests.post(upload_url, files={'post.png': open(img_path, 'rb')}).text
-    photos_data = json.loads(photos_json)['photos']
-    for photo_id in photos_data.keys():
-        token = photos_data[photo_id]['token']
-        attachment = {
-            'media': [
-                {
-                    'type': 'photo',
-                    'list': [
-                        {'photo_id': token}
-                    ]
-                }
-            ]
-        }
-        params = {
-            'gid': '53233370661082',
-            'type': 'GROUP_THEME',
-            'attachment': json.dumps(attachment)
-        }
-        print(api_request('mediatopic.post', params))
-
+    photo_id = json.loads(response_json)['photo_ids'][0]
+    uplaod_res = json.loads(requests.post(upload_url, files={'post.png': open(img_path, 'rb')}).text)
+    photo_token = uplaod_res['photos'][photo_id]['token']
+    attachment = {
+        'media': [
+            {
+                'type': 'photo',
+                'list': [
+                    {'id': photo_token}
+                ]
+            }
+        ]
+    }
+    params = {
+        'gid': '53233370661082',
+        'type': 'GROUP_THEME',
+        'attachment': json.dumps(attachment)
+    }
+    result = api_request('mediatopic.post', params)
+    return "error_code" not in json.loads(result).keys()
 
 if __name__ == '__main__':
     post()
